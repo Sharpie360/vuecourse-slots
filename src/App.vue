d23c140a6847e31b0eec654348423fdf154a0dd9
SeanBecker <techtronixwebdesign@gmail.com>
2018-10-10 22:36:10 -0500

    added - styles, new font, and slot names

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,8 @@
         <div class="row">
             <div>
                <app-quote class="cmp--Quote">
-                   <h1>{{ quoteTitle }}</h1>
-                   <h4>...not quite yet</h4>
+                   <h1 class="cmp--Quote-title" slot="title">{{ quoteTitle }}</h1>
+                   <h4 class="cmp--Quote-content" slot="content">...not quite yet</h4>
                </app-quote>
             </div>
         </div>
@@ -26,24 +26,50 @@
 </script>
 
 <style>
-    @import url('https://fonts.googleapis.com/css?family=Kodchasan:200,300,400,700');
+@import url('https://fonts.googleapis.com/css?family=Titillium+Web:200,300,400,700');
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
     html {
         font-size: 24px;
     }
     body {
-        font-family: 'Kodchasen', serif;
+        font-family: 'Titillium Web', serif;
         color: #f7f7f7;
         background-color: #006473;
+        display: flex;
+        flex: 1;
+        justify-content: center
+    }
+    h1, h2, h3, h4, h5, h6 {
+        font-weight: 200;
+        margin: .5em 0;
     }
 
+    .container {
+        width: 70%;
+    }
     .cmp--Quote {
         background-color: #03A9AC;
-        border: 2px solid #212121;
-
+        margin: 1rem 0;
         padding: 1rem;
-        width: 1140px;
+
+        width: 100%;
         text-align: center;
-        margin: 1rem auto;
+
+        box-shadow: 0px 0px 25px rgba(247, 247, 247, .0);
+        transition: box-shadow 0.1s;
+
+        border-radius: .75rem;
+    }
+    .cmp--Quote:hover {
+        box-shadow: 0px 0px 25px rgba(247, 247, 247, .5);
+    }
+
+    .cmp--Quote-content {
+        font-weight: 400;
     }
 
 </style>
