9f4037465b3130ef63fe2bb3a63085cf1bb0ec84
SeanBecker <techtronixwebdesign@gmail.com>
2018-10-11 22:38:29 -0500

    added - New cmp : demonstrates destroyed lifecycle hook

---
+++ src/components/New.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h3>New Quote</h3>
+    <button @click="counter++">Plus One</button>
+    {{ counter }}
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        counter: 0
+      }
+    },
+    destroyed () {
+      console.log('component destroyed')
+    }
+  }
+</script>
+
+<style>
+
+</style>
+
