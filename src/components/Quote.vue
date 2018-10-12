177bcaa65c136b97963a1f9db704d9ba9b893f8d
SeanBecker <techtronixwebdesign@gmail.com>
2018-10-11 22:43:33 -0500

    updated - moved styling to cmp scope

--- src/components/Quote.vue
+++ src/components/Quote.vue
@@ -18,7 +18,27 @@
 
 <style scoped>
   .outer {
-    border: 1rem solid #212121
+    border: 1rem solid #212121;
+    background-color: #03A9AC;
+    margin: 1rem 0;
+    padding: 1rem;
+
+    width: 100%;
+
+    box-shadow: 0px 0px 25px rgba(247, 247, 247, .0);
+    transition: box-shadow 0.1s;
+
+    border-radius: .75rem;
+  }
+  .outer:hover {
+    box-shadow: 0px 0px 25px rgba(247, 247, 247, .5);
+  }
+  .title {
+    text-shadow: 1px 1px 5px rgba(22, 22, 22, 0.3)
+  }
+  .content {
+    font-weight: 400;
+    color: rgb(53, 53, 53);
   }
 
 </style>
